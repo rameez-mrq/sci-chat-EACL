7ee52acb39e9ec5014ba5d05ffab87efc525fa04
rameez-mrq <rameez.mrq@gmail.com>
2024-04-09 15:19:24 +0100

    removed irrelevant comments

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,4 @@
 from start_server import app as application
-# from blender_start_server import app as application
 from flask_cors import CORS
 import torch
 CORS(application)
@@ -8,4 +7,3 @@ if __name__ == "__main__":
     # set_start_method('spawn')
     torch.multiprocessing.set_start_method('spawn', force = True)
     application.run()
-# #uWSGI @ file:///home/conda/feedstock_root/build_artifacts/uwsgi_1697223254948/work
